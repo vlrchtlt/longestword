12bfac01e33b0f0b42fcc812121bbddb6cf364e1
Valerie Chatelet <vlrchtlt@gmail.com>
2015-08-18 15:14:01 +0200

    index et show DONE

--- todo/config/routes.rb
+++ todo/config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
+
+  resources :tasks
+  # get 'tasks/' => 'tasks#index'
+
+  # post 'tasks/'  => 'tasks#create'
+
+  # get 'tasks/:id' => 'tasks#show'
+
+  # patch 'tasks/:id'    => 'tasks#update'
+
+  # delete 'tasks/:id'   => 'tasks#destroy'
+
+  # get 'tasks/:id/edit' => 'tasks#edit'
+
+  # get 'tasks/new' => 'tasks#new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- todo/db/seeds.rb
+++ todo/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+task_shop = Task.create(text: 'faire les courses', status: false)
+task_order = Task.create( text: 'ranger', status: false)
